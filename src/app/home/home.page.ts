e2024909d03e49c1941c7b910b9fc23f0efa3be8
Guilherme Munerato <50157442+guilhermemunerato@users.noreply.github.com>
2020-02-17 20:25:57 -0300

    Lista de Pokemons por Códigos
    Ajustado a interface para percorrer uma lista dinâmica de Pokemons.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,58 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public listaPokemons = [
+    {
+      numero: "001", nome: "Bulbasaur",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    },
+
+    {
+      numero: "002", nome: "charmander",
+      tipos: ["Grass", "Poison"],
+      img:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    },
+
+
+    {
+      numero: "003", nome: "Stakataka",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    },
+
+    {
+      numero: "004", nome: "Metapod",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png",
+    },
+    {
+      numero: "005", nome: "INDIANA JONES",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/808.png",
+    },
+    {
+      numero: "006", nome: "MALOKA",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/802.png",
+    },
+    {
+      numero: "007", nome: "POPE",
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/799.png",
+    },
+    {
+      numero: "008", nome: 'ARIZONA',
+      tipos: ["Grass", "Poison"],
+      img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/805.png",
+    },
+     
+  ];
+
+  public listaFiltrada = [];
+
+
+  contructor() { }
 
 }
+
