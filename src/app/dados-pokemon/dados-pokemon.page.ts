93694273f2e4c13741467f7ee8933ca0090edda3
Guilherme Munerato <50157442+guilhermemunerato@users.noreply.github.com>
2020-03-02 20:30:49 -0300

    .

--- src/app/dados-pokemon/dados-pokemon.page.ts
+++ src/app/dados-pokemon/dados-pokemon.page.ts
@@ -23,3 +23,4 @@ export class DadosPokemonPage implements OnInit {
   }
 
 }
+ 
---
+++ src/app/servicos/pokedex-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokedexApiService {
+  private url = "https://pokeapi.co/api/v2/pokemon"
+
+  constructor(public http:HttpClient) { }
+
+  public listarPokemons(offset:number,limit = 20) {
+
+    /// reduzir e chamar `crase`
+return this.http.get(`${this.url}/?offset=${offset}&limit=${limit}`);
+  }
+public buscaPornumero(numero: Number){
+  return this.http.get(`${this.url}/${numero}
+  `);
+}
+  
+}
